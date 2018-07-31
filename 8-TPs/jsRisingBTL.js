b677290c42d9f71b2d09532f903b8896204cd2c1
frgen <fede_genesis@hotmail.com>
2018-07-30 23:42:14 -0300

    Ejercicio de TP (12) cambios

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -16,49 +16,83 @@ function ComenzarIngreso ()
     var NumLegajo;
     var nacionalidad;
 
-    edad = prompt("edad");
+    edad = prompt("Ingrese la edad");
     edad = parseInt(edad);
 
     while (edad < 18 || edad > 90) {
         edad = prompt("Edad incorrecta, ingrese una edad entre 18 y 90 años inclusive");
     }
 
-    sexo = prompt("sexo");
+    sexo = prompt("Ingrese el sexo");
+    sexo = sexo.toUpperCase();
 
-    while (sexo != "m" && sexo != "f") {
-        sexo = prompt("Sexo incorrecto, ingrese 'm' o 'f' ");
+    while (sexo != "M" && sexo != "F") {
+        sexo = prompt("Sexo incorrecto, ingrese 'M' o 'F' ");
+        sexo = sexo.toUpperCase();
     }
 
-    EstadoCivil = prompt("Estado civil");
+    EstadoCivil = prompt("Ingrese el estado civil");
     EstadoCivil = parseInt(EstadoCivil);
 
     while (EstadoCivil != 1 && EstadoCivil != 2 && EstadoCivil != 3 && EstadoCivil != 4) {
         EstadoCivil = prompt("Opción incorrecta, ingrese 1 soltero, 2 casado, 3 divorciado ó 4 viudo");
     }
 
-    SueldoBruto = prompt("Sueldo bruto");
+    SueldoBruto = prompt("Ingrese el sueldo bruto");
 
     while (SueldoBruto < 8000) {
         SueldoBruto = prompt("El sueldo bruto no debe ser menor a 8000");
     }
 
-    NumLegajo = prompt("Numero de legajo");
+    NumLegajo = prompt("Ingrese el numero de legajo");
     NumLegajo = parseInt(NumLegajo);
 
     while (NumLegajo < 1000) {
         NumLegajo = prompt("El nº de legajo no debe ser menor a 1000");
     }
 
-    nacionalidad = prompt("Nacionalidad");
+    nacionalidad = prompt("Ingrese la nacionalidad");
+    nacionalidad = nacionalidad.toUpperCase();
 
-    while (nacionalidad != "a" && nacionalidad != "e" && nacionalidad != "n") {
-        nacionalidad = prompt("Ingrese a argentinos, e extranjeros o n nacionalizados");
+    while (nacionalidad != "A" && nacionalidad != "E" && nacionalidad != "N") {
+        nacionalidad = prompt("Ingrese A argentinos, E extranjeros o N nacionalizados");
+        nacionalidad = nacionalidad.toUpperCase();
     }
 
-    document.getElementById("Edad").value = edad;
+    if (sexo == "F") {
+        sexo = "Femenino";
+    } else {
+        sexo = "Masculino";
+    }
+
+    switch(EstadoCivil) {
+        case "1":
+            EstadoCivil = "Soltero/a";
+        break;
+        case "2":
+            EstadoCivil = "Casado/a";
+        break;
+        case "3":
+            EstadoCivil = "Divorciado/a";
+        break;
+        default:
+            EstadoCivil = "Viudo/a";
+    }
+
+    switch(nacionalidad) {
+        case "A":
+            nacionalidad = "Argentino/a";
+        break;
+        case "E":
+            nacionalidad = "Extranjero/a";
+        default:
+            nacionalidad = "Nacionalizado/a";
+    }
+
+    document.getElementById("Edad").value = edad + " años";
     document.getElementById("Sexo").value = sexo;
     document.getElementById("EstadoCivil").value = EstadoCivil;
-    document.getElementById("Sueldo").value = SueldoBruto;
+    document.getElementById("Sueldo").value = "$" + SueldoBruto;
     document.getElementById("Legajo").value = NumLegajo;
     document.getElementById("Nacionalidad").value = nacionalidad;
  
